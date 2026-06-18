9126fa3896b9ee3bfa4e71d2ac9658113de7657e
Sheikh Nahian <sheiknahian06@gmail.com>
2026-06-18 17:31:49 +0600

    fixed-1

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const {MongoClient, ObjectId, ServerApiVersion} = require('mongodb')
 const dotenv = require('dotenv')
 const admin = require("firebase-admin");
 
+dotenv.config()
+console.log("KEY:", process.env.DB_PASS);
 const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
 const serviceAccount = JSON.parse(decoded);
 
-dotenv.config()
 
 const stripe =  require('stripe')(process.env.STRIPE_SECRET);
 
@@ -15,7 +16,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors())
-
 const PORT = process.env.PORT || 5000;
 
 admin.initializeApp({
